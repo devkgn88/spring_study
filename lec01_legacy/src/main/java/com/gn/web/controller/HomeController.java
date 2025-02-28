cc181b9bab620cf91deb5ffc46ba728ce63a9e07
devkgn88 <devkgn88@gmail.com>
2025-02-28 17:56:37 +0900

    [kgn] 홈 컨트롤러 코드 수정

--- lec01_legacy/src/main/java/com/gn/web/controller/HomeController.java
+++ lec01_legacy/src/main/java/com/gn/web/controller/HomeController.java
@@ -3,13 +3,11 @@ package com.gn.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-	
-	// 클라이언트가 요청한 서비스 주소와 메소드 연결
-	@GetMapping({"","/"})
-	public String home() {
-		return "home";
+	@Controller
+	public class HomeController {
+		// 클라이언트가 요청한 서비스 주소와 메소드 연결
+		@GetMapping({"","/"})
+		public String home() {
+			return "home";
+		}
 	}
-
-}
