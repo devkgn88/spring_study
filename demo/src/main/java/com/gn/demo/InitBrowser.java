1ff3201f1dfa20387669483b9e1696ec68977815
devkgn88 <devkgn88@gmail.com>
2025-03-04 15:36:02 +0900

    [kgn] Spring Boot 프로젝트 생성

---
+++ demo/src/main/java/com/gn/demo/InitBrowser.java
@@ -0,0 +1,24 @@
+package com.gn.demo;
+
+import java.awt.Desktop;
+import java.net.URI;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class InitBrowser {
+
+	@PostConstruct
+	public void init() {
+		String url ="http://localhost:8080";
+		System.setProperty("java.awt.headless","false");
+		try {
+			Desktop.getDesktop().browse(URI.create(url));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		 
+	}
+}
