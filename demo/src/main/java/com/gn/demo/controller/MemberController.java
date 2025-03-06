6591ed567e5ae0d6db3d3b5933debbad2acbcb70
devkgn88 <devkgn88@gmail.com>
2025-03-06 14:13:45 +0900

    [kgn] thymeleaf 맛보기 코드 추가

--- demo/src/main/java/com/gn/demo/controller/MemberController.java
+++ demo/src/main/java/com/gn/demo/controller/MemberController.java
@@ -1,8 +1,23 @@
 package com.gn.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gn.demo.vo.Member;
 
 @Controller
 public class MemberController {
-	public String 
+
+	@GetMapping("/memberList")
+	public String selectMemberList(Model model) {
+		List<Member> list = new ArrayList<Member>();
+		list.add(new Member("김철수",25));
+		list.add(new Member("이영희",17));
+		model.addAttribute("members", list);
+		return "/member/list";
+	}
 }
