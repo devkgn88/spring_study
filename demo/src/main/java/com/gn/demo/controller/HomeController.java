a9674702d16f642131f80b0006ee7541737eb399
devkgn88 <devkgn88@gmail.com>
2025-03-06 17:01:49 +0900

    [kgn]goodbye 페이지 구성

--- demo/src/main/java/com/gn/demo/controller/HomeController.java
+++ demo/src/main/java/com/gn/demo/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.gn.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gn.demo.vo.Member;
 
 @Controller
 public class HomeController {
 
 	@GetMapping({"","/"})
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("member", new Member("홍길동",50));
 		return "home";
 	}
 	
@@ -15,5 +19,12 @@ public class HomeController {
 	public String testView() {
 		return "test";
 	}
+	
+	@GetMapping("bye")
+	public String byeMember(Model model) {
+		model.addAttribute("member",new Member("홍길동",50));
+		return "goodbye";
+	}
+	
 	
 }
