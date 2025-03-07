9388583502c3e6e83bcf4e7d175b389bb5cdef2e
devkgn88 <devkgn88@gmail.com>
2025-03-07 16:48:02 +0900

    [kgn] thymeleaf dialect layout 반영

---
+++ mvc/src/main/java/com/gn/mvc/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gn.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping({"/",""})
+	public String home() {
+		return "home";
+	}
+}
+
