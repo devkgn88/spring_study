ef88e48d225b4d9fa4719d578f34e8d45e9c218c
devkgn88 <devkgn88@gmail.com>
2025-03-10 16:58:43 +0900

    [kgn] Dto 방식으로 변경

--- mvc/src/main/java/com/gn/mvc/controller/BoardController.java
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gn.mvc.dto.BoardDto;
 
 @Controller
 public class BoardController {
@@ -17,17 +19,29 @@ public class BoardController {
 		return "board/create";
 	}
 	
+//	@PostMapping("/board/create")
+//	@ResponseBody
+//	public Map<String,String> createBoardApi(
+//			@RequestParam("board_title") String boardTitle,
+//			@RequestParam("board_content") String boardContent){
+//		Map<String,String> resultMap = new HashMap<String,String>();
+//		resultMap.put("res_code", "404");
+//		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
+//		
+//		System.out.println(boardTitle);
+//		System.out.println(boardContent);
+//		
+//		return resultMap;
+//	}
+	
 	@PostMapping("/board/create")
 	@ResponseBody
-	public Map<String,String> createBoardApi(
-			@RequestParam("board_title") String boardTitle,
-			@RequestParam("board_content") String boardContent){
+	public Map<String,String> createBoardApi(BoardDto dto){
 		Map<String,String> resultMap = new HashMap<String,String>();
 		resultMap.put("res_code", "404");
 		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
 		
-		System.out.println(boardTitle);
-		System.out.println(boardContent);
+		System.out.println(dto);
 		
 		return resultMap;
 	}
