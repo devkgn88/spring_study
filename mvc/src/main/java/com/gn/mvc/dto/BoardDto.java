7495e978362661581c8d666862e36ef17e27e96e
devkgn88 <devkgn88@gmail.com>
2025-03-10 17:47:57 +0900

    [kgn] Board 엔티티 생성 및 BoardDto 수정

--- mvc/src/main/java/com/gn/mvc/dto/BoardDto.java
+++ mvc/src/main/java/com/gn/mvc/dto/BoardDto.java
@@ -1,6 +1,9 @@
 package com.gn.mvc.dto;
 
+import com.gn.mvc.entity.Board;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +14,25 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class BoardDto {
+	private Long board_no;
 	private String board_title;
 	private String board_content;
+	
+	public Board toEntity() {
+		return Board.builder()
+				.boardTitle(board_title)
+				.boardContent(board_content)
+				.boardNo(board_no)
+				.build();
+	}
+	
+	public BoardDto toDto(Board board){
+		return BoardDto.builder()
+				.board_title(board.getBoardTitle())
+				.board_content(board.getBoardContent())
+				.board_no(board.getBoardNo())
+				.build();
+	}
 }
