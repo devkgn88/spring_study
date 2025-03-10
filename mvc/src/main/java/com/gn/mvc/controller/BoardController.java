206b0503fe9513ac24d90c5875a4e0d3849bd386
devkgn88 <devkgn88@gmail.com>
2025-03-10 16:51:56 +0900

    [kgn] 백엔드로 데이터 전달

--- mvc/src/main/java/com/gn/mvc/controller/BoardController.java
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -1,7 +1,13 @@
 package com.gn.mvc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -9,5 +15,20 @@ public class BoardController {
 	@GetMapping("/board/create")
 	public String createBoardView() {
 		return "board/create";
+	}
+	
+	@PostMapping("/board/create")
+	@ResponseBody
+	public Map<String,String> createBoardApi(
+			@RequestParam("board_title") String boardTitle,
+			@RequestParam("board_content") String boardContent){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "404");
+		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
+		
+		System.out.println(boardTitle);
+		System.out.println(boardContent);
+		
+		return resultMap;
 	}
 }
