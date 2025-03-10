f876a647f48528fab562b74e4e9e76b95bc06e93
devkgn88 <devkgn88@gmail.com>
2025-03-10 17:58:12 +0900

    [kgn] 게시글 등록 기능 구현

--- mvc/src/main/java/com/gn/mvc/controller/BoardController.java
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.gn.mvc.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.BoardDto;
 import com.gn.mvc.entity.Board;
+import com.gn.mvc.repository.BoardRepository;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardRepository boardRepository;
 	
 	@GetMapping("/board/create")
 	public String createBoardView() {
@@ -34,6 +39,7 @@ public class BoardController {
 //		return resultMap;
 //	}
 	
+	
 	@PostMapping("/board/create")
 	@ResponseBody
 	public Map<String,String> createBoardApi(BoardDto dto){
@@ -41,7 +47,15 @@ public class BoardController {
 		resultMap.put("res_code", "404");
 		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
 		
+		// 1. DTO를 엔티티로 변환
 		Board board = dto.toEntity();
+		// 2. DTO가 엔티티로 잘 변환되었는지 확인
+		System.out.println(board);
+		// 3. 레포지토리로 엔티티를 DB에 저장
+		Board saved = boardRepository.save(board);
+		// 4. board가 DB에 잘 저장되는지 확인
+		System.out.println(saved);
+		
 		
 		return resultMap;
 	}
