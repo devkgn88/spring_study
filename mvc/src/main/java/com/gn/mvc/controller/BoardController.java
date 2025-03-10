64af72b17f1b6d2150eff2b9b9655b174b93080f
devkgn88 <devkgn88@gmail.com>
2025-03-10 15:10:41 +0900

    [kgn] 게시글 등록 화면 이동

---
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.gn.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping("/board/create")
+	public String createBoardView() {
+		return "board/create";
+	}
+}
