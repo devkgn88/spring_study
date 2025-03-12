7e18d7757abea2dcd0e6a8c29a17242aac053e8e
devkgn88 <devkgn88@gmail.com>
2025-03-12 13:09:36 +0900

    [kgn] 날짜 관련 필드 추가

--- mvc/src/main/java/com/gn/mvc/controller/BoardController.java
+++ mvc/src/main/java/com/gn/mvc/controller/BoardController.java
@@ -3,15 +3,14 @@ package com.gn.mvc.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.BoardDto;
-import com.gn.mvc.entity.Board;
-import com.gn.mvc.repository.BoardRepository;
 import com.gn.mvc.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +20,8 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 	
 	private final BoardService boardService;
+	
+	private Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
 	@GetMapping("/board/create")
 	public String createBoardView() {
@@ -51,7 +52,7 @@ public class BoardController {
 		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
 		
 		BoardDto saved = boardService.createBoard(dto);
-		
+		logger.info(saved.toString());
 		if(saved.getBoard_no() != null) {
 			resultMap.put("res_code", "200");
 			resultMap.put("res_msg", "게시글이 등록되었습니다.");
--- mvc/src/main/java/com/gn/mvc/entity/Board.java
+++ mvc/src/main/java/com/gn/mvc/entity/Board.java
@@ -1,5 +1,10 @@
 package com.gn.mvc.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -31,5 +36,13 @@ public class Board {
 	
 	@Column(name="board_content")
 	private String boardContent;
+	
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime regDate;
+	
+	@UpdateTimestamp
+	@Column(insertable = false)
+	private LocalDateTime modDate;
 	
 }
