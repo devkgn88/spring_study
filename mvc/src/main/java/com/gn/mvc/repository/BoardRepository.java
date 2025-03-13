0c29f3768abe893ae59770ef686d284bb7be704c
devkgn88 <devkgn88@gmail.com>
2025-03-13 10:00:09 +0900

    [kgn] Specification 설정

--- mvc/src/main/java/com/gn/mvc/repository/BoardRepository.java
+++ mvc/src/main/java/com/gn/mvc/repository/BoardRepository.java
@@ -2,31 +2,36 @@ package com.gn.mvc.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.gn.mvc.entity.Board;
 
-public interface BoardRepository extends JpaRepository<Board,Long>{
+public interface BoardRepository extends JpaRepository<Board,Long>, JpaSpecificationExecutor<Board>{
+	
+	// 1. 메소드 네이밍
 //	List<Board> findByBoardTitleContaining(String keyword);
 //	
 //	List<Board> findByBoardContentContaining(String keyword);
 //	
 //  List<Board> findByBoardTitleContainingOrBoardContentContaining(String titleKeyword, String contentKeyword);
 	
-	@Query(value="SELECT b FROM Board b "+
-	"WHERE b.boardTitle LIKE CONCAT('%',?1,'%')")
-	List<Board> findByBoardTitleContaining(String keyword);
-	
-	@Query(value="SELECT b FROM Board b "+
-	"WHERE b.boardContent LIKE CONCAT('%',?1,'%')")
-	List<Board> findByBoardContentContaining(String keyword);
-	
-	@Query(value="SELECT b FROM Board b "+
-	"WHERE b.boardTitle LIKE CONCAT('%',?1,'%') "+
-	"OR b.boardContent LIKE CONCAT('%',?2,'%')")
-	List<Board> findByBoardTitleContainingOrBoardContentContaining(String titleKeyword, String contentKeyword);
+	// 2. JPQL
+//	@Query(value="SELECT b FROM Board b "+
+//	"WHERE b.boardTitle LIKE CONCAT('%',?1,'%')")
+//	List<Board> findByBoardTitleContaining(String keyword);
+//	
+//	@Query(value="SELECT b FROM Board b "+
+//	"WHERE b.boardContent LIKE CONCAT('%',?1,'%')")
+//	List<Board> findByBoardContentContaining(String keyword);
+//	
+//	@Query(value="SELECT b FROM Board b "+
+//	"WHERE b.boardTitle LIKE CONCAT('%',?1,'%') "+
+//	"OR b.boardContent LIKE CONCAT('%',?2,'%')")
+//	List<Board> findByBoardTitleContainingOrBoardContentContaining(String titleKeyword, String contentKeyword);
 			
-	
+	// 3. Specification
+	List<Board> findAll(Specification<Board> spec);
 
 }
--- mvc/src/main/java/com/gn/mvc/service/BoardService.java
+++ mvc/src/main/java/com/gn/mvc/service/BoardService.java
@@ -3,12 +3,14 @@ package com.gn.mvc.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.gn.mvc.dto.BoardDto;
 import com.gn.mvc.dto.SearchDto;
 import com.gn.mvc.entity.Board;
 import com.gn.mvc.repository.BoardRepository;
+import com.gn.mvc.specification.BoardSpecification;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,18 +22,23 @@ public class BoardService {
 	
 	public List<Board> selectBoardAll(SearchDto dto){
 		List<Board> list = new ArrayList<Board>();
+		Specification<Board> spec = (root,query,criteriaBuilder) -> null; 
 		if(dto.getSearch_type() == 1) {
-			list = boardRepository.findByBoardTitleContaining(dto.getSearch_text());	
-			System.out.println("제목 : "+list);
+//			list = boardRepository.findByBoardTitleContaining(dto.getSearch_text());	
+//			System.out.println("제목 : "+list);
+			spec = spec.and(BoardSpecification.boardTitleContains(dto.getSearch_text()));
 		}else if(dto.getSearch_type() == 2) {
-			list = boardRepository.findByBoardContentContaining(dto.getSearch_text());
-			System.out.println("내용 : "+list);
+//			list = boardRepository.findByBoardContentContaining(dto.getSearch_text());
+//			System.out.println("내용 : "+list);
+			spec = spec.and(BoardSpecification.boardContentContains(dto.getSearch_text()));
 		} else if(dto.getSearch_type() == 3) {
-			list = boardRepository.findByBoardTitleContainingOrBoardContentContaining(dto.getSearch_text(),dto.getSearch_text());
-			System.out.println("제목+내용 : "+list);
-		} else {
-			list = boardRepository.findAll();
+//			list = boardRepository.findByBoardTitleContainingOrBoardContentContaining(dto.getSearch_text(),dto.getSearch_text());
+//			System.out.println("제목+내용 : "+list);
+			spec = spec.and(BoardSpecification.boardTitleContains(dto.getSearch_text()))
+					.or(BoardSpecification.boardContentContains(dto.getSearch_text()));
 		}
+		list = boardRepository.findAll(spec);
+		
 		return list;
 	}
 	
