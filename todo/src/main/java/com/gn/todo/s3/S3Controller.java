dd28785539534834aa5924c6dd8104c2fe20b56b
devkgn88 <devkgn88@gmail.com>
2025-03-31 20:52:12 +0900

    [kgn] 테스트 13

--- todo/src/main/java/com/gn/todo/s3/S3Controller.java
+++ todo/src/main/java/com/gn/todo/s3/S3Controller.java
@@ -42,7 +42,7 @@ public class S3Controller {
 	
 	@GetMapping("/download/{id}")
 	public ResponseEntity<Object> downloadFile(@PathVariable("id") Long id) {
-		System.out.println("테스트12:다양한 것 수정");
+		System.out.println("테스트13");
 		try {
 			// 1. 파일 정보 조회
 			Attach fileData = attachService.selectAttachOne(id);
