c7146674b56b4ebd4ba38d99b7d1952bc542a306
devkgn88 <devkgn88@gmail.com>
2025-04-01 19:23:35 +0900

    [kgn] 테스트 70번

--- todo/src/main/java/com/gn/todo/s3/S3Controller.java
+++ todo/src/main/java/com/gn/todo/s3/S3Controller.java
@@ -37,7 +37,7 @@ public class S3Controller {
 	
 	@GetMapping("/download/{id}")
 	public ResponseEntity<Object> downloadFile(@PathVariable("id") Long id) {
-		System.out.println("테스트60");
+		System.out.println("테스트70");
 		try {
 			// 1. 파일 정보 조회
 			Attach fileData = attachService.selectAttachOne(id);
