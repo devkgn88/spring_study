8914a90e313db4bfed92b89f76c89ae28a6ccb33
devkgn88 <devkgn88@gmail.com>
2025-04-01 22:06:58 +0900

    20

--- todo/src/main/java/com/gn/todo/s3/S3Controller.java
+++ todo/src/main/java/com/gn/todo/s3/S3Controller.java
@@ -37,7 +37,7 @@ public class S3Controller {
 	
 	@GetMapping("/download/{id}")
 	public ResponseEntity<Object> downloadFile(@PathVariable("id") Long id) {
-		System.out.println("jenkins test19");
+		System.out.println("jenkins test20");
 		try {
 			// 1. 파일 정보 조회
 			Attach fileData = attachService.selectAttachOne(id);
