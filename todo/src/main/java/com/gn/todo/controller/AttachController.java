cd4dc3df5bf2d3bf3d092deaf57eb50bbab8b4eb
devkgn88 <devkgn88@gmail.com>
2025-04-01 17:45:52 +0900

    [kgn] 테스트9

--- todo/src/main/java/com/gn/todo/controller/AttachController.java
+++ todo/src/main/java/com/gn/todo/controller/AttachController.java
@@ -36,7 +36,7 @@ public class AttachController {
 	  
 	@GetMapping("/download/{id}") 
 	public ResponseEntity<Object> fileDownload(@PathVariable("id") Long id) {
-		System.out.println("테스트8");
+		System.out.println("테스트9");
 		try {
 			Attach fileData = attachService.selectAttachOne(id);
 			if(fileData == null) {
